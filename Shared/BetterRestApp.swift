0ea7d0be4d060babe2308326359d1d1dc415c496
Brando Lugo <brando.lugo@gmail.com>
2020-12-21 17:22:56 -0600

    Initial Commit

---
+++ Shared/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  Shared
+//
+//  Created by Brando Lugo on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
